93772e108a3376ee020068fd28f7f70afd975a93
Oleg <reffum@bk.ru>
2023-05-28 19:42:12 +0300

    In PdfContentItemModel move Tree and TreeElement inside PdfContentItemModel.
    This is done for avoid names conflicts.

--- pdfcontentitemmodel.h
+++ pdfcontentitemmodel.h
@@ -7,126 +7,126 @@
 #include <poppler/qt5/poppler-qt5.h>
 
 //
-// The structure to store Poppler::OutlineItem elements in tree form
-//
-
-class TreeElement
-{
-public:
-    explicit TreeElement(Poppler::OutlineItem item):
-        outlineItem(item),
-        parent(nullptr)
-    {
-
-    }
-
-    explicit TreeElement():
-        parent(nullptr)
-    {
-
-    }
-
-    void appendChild(TreeElement * elem)
-    {
-        childrens.push_back(elem);
-        elem->parent = this;
-        elem->parentIndex = childrens.size() - 1;
-    }
-
-    TreeElement * getParent() const
-    {
-        return parent;
-    }
-
-    bool hasChildren()
-    {
-        return !childrens.empty();
-    }
-
-    int getChildNum() const
-    {
-        return childrens.size();
-    }
-
-    TreeElement * getChild(const int i) const
-    {
-        return childrens[i];
-    }
-
-    Poppler::OutlineItem getOutline() const
-    {
-        return outlineItem;
-    }
-
-    int getParentIndex() const
-    {
-        return parentIndex;
-    }
-
-private:
-    Poppler::OutlineItem outlineItem;
-    TreeElement * parent;
-    QVector<TreeElement*> childrens;
-
-    // Index of this TreeElement in its parent list
-    int parentIndex = 0;
-};
-
-class Tree
-{
-public:
-    Tree(QVector<Poppler::OutlineItem> outlines)
-    {
-        root = new TreeElement();
-        createTreeFor(outlines, root);
-    }
-
-    ~Tree()
-    {
-        deleteWithChildren(root);
-    }
-
-    TreeElement * getRoot() const
-    {
-        return root;
-    }
-
-private:
-    TreeElement * root;
-
-    // Traverse outlineItems tree and create
-    // the same tree with parent as root element
-    void createTreeFor(QVector<Poppler::OutlineItem> outlines, TreeElement * parent)
-    {
-        Poppler::OutlineItem outlineItem;
-        foreach(outlineItem, outlines)
-        {
-            TreeElement * elem = new TreeElement(outlineItem);
-            parent->appendChild(elem);
-
-            if(outlineItem.hasChildren())
-                createTreeFor(outlineItem.children(), elem);
-        }
-    }
-
-    // Delete TreeElement and its children
-    void deleteWithChildren(TreeElement * elem)
-    {
-        for(int i = 0; i < elem->getChildNum(); i++)
-        {
-            TreeElement * childElem = elem->getChild(i);
-            deleteWithChildren(childElem);
-        }
-
-        delete elem;
-    }
-};
-
-//
 // Items model for QTreeView show contents of document
 //
 class PdfContentItemModel : public ContentsItemModel
 {
+    //
+    // The structure to store Poppler::OutlineItem elements in tree form
+    //
+
+    class TreeElement
+    {
+    public:
+        explicit TreeElement(Poppler::OutlineItem item):
+            outlineItem(item),
+            parent(nullptr)
+        {
+
+        }
+
+        explicit TreeElement():
+            parent(nullptr)
+        {
+
+        }
+
+        void appendChild(TreeElement * elem)
+        {
+            childrens.push_back(elem);
+            elem->parent = this;
+            elem->parentIndex = childrens.size() - 1;
+        }
+
+        TreeElement * getParent() const
+        {
+            return parent;
+        }
+
+        bool hasChildren()
+        {
+            return !childrens.empty();
+        }
+
+        int getChildNum() const
+        {
+            return childrens.size();
+        }
+
+        TreeElement * getChild(const int i) const
+        {
+            return childrens[i];
+        }
+
+        Poppler::OutlineItem getOutline() const
+        {
+            return outlineItem;
+        }
+
+        int getParentIndex() const
+        {
+            return parentIndex;
+        }
+
+    private:
+        Poppler::OutlineItem outlineItem;
+        TreeElement * parent;
+        QVector<TreeElement*> childrens;
+
+        // Index of this TreeElement in its parent list
+        int parentIndex = 0;
+    };
+
+    class Tree
+    {
+    public:
+        Tree(QVector<Poppler::OutlineItem> outlines)
+        {
+            root = new TreeElement();
+            createTreeFor(outlines, root);
+        }
+
+        ~Tree()
+        {
+            deleteWithChildren(root);
+        }
+
+        TreeElement * getRoot() const
+        {
+            return root;
+        }
+
+    private:
+        TreeElement * root;
+
+        // Traverse outlineItems tree and create
+        // the same tree with parent as root element
+        void createTreeFor(QVector<Poppler::OutlineItem> outlines, TreeElement * parent)
+        {
+            Poppler::OutlineItem outlineItem;
+            foreach(outlineItem, outlines)
+            {
+                TreeElement * elem = new TreeElement(outlineItem);
+                parent->appendChild(elem);
+
+                if(outlineItem.hasChildren())
+                    createTreeFor(outlineItem.children(), elem);
+            }
+        }
+
+        // Delete TreeElement and its children
+        void deleteWithChildren(TreeElement * elem)
+        {
+            for(int i = 0; i < elem->getChildNum(); i++)
+            {
+                TreeElement * childElem = elem->getChild(i);
+                deleteWithChildren(childElem);
+            }
+
+            delete elem;
+        }
+    };
+
 public:
     explicit PdfContentItemModel(QVector<Poppler::OutlineItem>, QObject * parent = nullptr);
 
